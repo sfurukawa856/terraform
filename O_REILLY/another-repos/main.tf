9a26411865491de4fa88765bb6455f185eb70068
sfurukawa <tennis856@gmail.com>
2024-08-05 12:12:41 +0900

    add webserver-cluster module

--- O_REILLY/another-repos/main.tf
+++ O_REILLY/another-repos/main.tf
@@ -5,3 +5,11 @@ module "network" {
   # source = "git@github.com:sfurukawa856/tf-modueles.git//network?ref=v0.0.1"
   project = var.project
 }
+
+module "webserver-cluster" {
+  source        = "github.com/sfurukawa856/tf-modueles//webserver-cluster?ref=v0.0.7"
+  project       = var.project
+  instance_type = "t2.micro"
+  subnet_ids    = [module.network.public_subnet_1a_id, module.network.public_subnet_1c_id]
+  vpc_id        = module.network.vpc_id
+}
