fd7a8d2ddfe5c9f404d7b432639eeccd2eb05cb4
sfurukawa <tennis856@gmail.com>
2024-08-05 12:12:06 +0900

    terraform fmt 実行

--- O_REILLY/backend.tf
+++ O_REILLY/backend.tf
@@ -30,10 +30,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 
 # 全パブリックアクセスをブロック
 resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.tf_state.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  bucket                  = aws_s3_bucket.tf_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
@@ -41,9 +41,9 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 # リモートバックエンド用DynamoDB
 # ------------------------------------
 resource "aws_dynamodb_table" "tf_state" {
-  name = "${var.project}-tf-state-lock"
+  name         = "${var.project}-tf-state-lock"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
--- O_REILLY/provider.tf
+++ O_REILLY/provider.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "o-reilly-tf-state-bucket"
-    key = "o-reilly/terraform.tfstate"
-    region = "ap-northeast-1"
+    bucket         = "o-reilly-tf-state-bucket"
+    key            = "o-reilly/terraform.tfstate"
+    region         = "ap-northeast-1"
     dynamodb_table = "o-reilly-tf-state-lock"
-    encrypt = true
+    encrypt        = true
   }
 }
