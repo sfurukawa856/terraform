0046c5b9b3a0878e3781d0da2bb97e4f8be650af
sfurukawa <tennis856@gmail.com>
2025-03-31 07:11:44 +0900

    delete private subnet

--- 202503_udemy/main.tf
+++ 202503_udemy/main.tf
@@ -4,7 +4,7 @@ module "network" {
   env                  = "dev"
   az_count             = 1
   public_subnet_count  = 1
-  private_subnet_count = 2
+  private_subnet_count = 1
   vpc_cidr             = "10.0.0.0/16"
   enable_nat_gateway   = false
 }
